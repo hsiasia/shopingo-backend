e141c95ce8588ee0c95d8bba1fdebb126c212a4b
蔡見昇 <jason_tsai@caijianshengdeMacBook-Air.local>
2024-04-10 16:13:25 +0800

    fix: using DRF instead of using plain SQL cursor

--- Django_framework/user/models.py
+++ Django_framework/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=16)
+    gmail = models.EmailField(max_length=256)
+    profile_pic = models.URLField(max_length=256)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.name
