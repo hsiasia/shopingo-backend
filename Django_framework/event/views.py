267559c642722b5d26cda17390f6fbe231a6cb5b
ZG926 <b07902089@ntu.edu.tw>
2024-04-14 04:55:17 +0800

    finished join_event

--- Django_framework/event/views.py
+++ Django_framework/event/views.py
@@ -5,8 +5,9 @@ from rest_framework import generics
 from drf_yasg.utils import swagger_auto_schema
 
 from .models import Event
+from .models import Participant
 from .serializers import GetEventSerializer
-from .serializers import CreateEventSerializer
+from .serializers import ParticipantSerializer
 
 from drf_yasg import openapi
 from rest_framework import status
@@ -47,6 +48,50 @@ class HandleEvent(generics.CreateAPIView):
         # Set event_date to the current date and time
         serializer.save(create_datetime=timezone.now())
         serializer.save(update_datetime=timezone.now())
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        print(request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            print(serializer.data)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class HandleEventInfo(generics.CreateAPIView):
+    queryset = Participant.objects.all()
+    serializer_class = ParticipantSerializer
+    def get(self, request, *args, **krgs):
+        event_id = request.query_params.get('event_id')
+        user_id = request.query_params.get('user_id')
+        if event_id: 
+            data = Participant.objects.filter(event_id=event_id).\
+                values(
+                    'event', 'user')
+            resp = {
+                'data': list(data),
+                'error': None,
+                'status': status.HTTP_200_OK, 
+            }
+            return JsonResponse(resp)
+        elif user_id: 
+            data = Participant.objects.filter(user_id=user_id).\
+                values(
+                    'event', 'user')
+            resp = {
+                'data': list(data),
+                'error': None,
+                'status': status.HTTP_200_OK, 
+            }
+            return JsonResponse(resp)
+        else: 
+            resp = {
+                'data': None,
+                'error': "No userId provided",
+                'status': status.HTTP_400_BAD_REQUEST, 
+            }
+            return JsonResponse(resp)
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
