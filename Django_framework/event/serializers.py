3deb97abf92d949732d2408a2db66b740fe436c6
ZG926 <b07902089@ntu.edu.tw>
2024-04-14 04:27:48 +0800

    finished get_all event get_event_by_id and create_event

---
+++ Django_framework/event/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Event
+from .models import Participant
+
+class GetEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['id', 'creator', 'event_name', 'company_name', 'hashtag', 'location', 'event_date', 'scale', 'budget', 'detail', 'create_datetime', 'update_datetime', 'delete_datetime', 'score']
+        extra_kwargs = {
+            'create_datetime': {'required': False},  # Set required to False to allow null values
+            'update_datetime': {'required': False},  # Set required to False to allow null values
+            'dalete_datetime': {'required': False},  # Set required to False to allow null values
+            'score': {'required': False}  # Set required to False to allow null values
+        }
+
+class CreateEventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['creator', 'event_name', 'company_name', 'hashtag', 'location', 'event_date', 'scale', 'budget', 'detail', 'create_datetime', 'update_datetime', 'delete_datetime', 'score']
+        extra_kwargs = {
+            'create_datetime': {'required': False},  # Set required to False to allow null values
+            'update_datetime': {'required': False},  # Set required to False to allow null values
+            'dalete_datetime': {'required': False},  # Set required to False to allow null values
+            'score': {'required': False}  # Set required to False to allow null values
+        }
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Participant
+        fields = ['event', 'user']
