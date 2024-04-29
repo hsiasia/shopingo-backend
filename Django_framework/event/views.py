5bb8805acc34e3e42d3c77a9f470c34cd7d35ff6
ZG926 <b07902089@ntu.edu.tw>
2024-04-29 23:38:12 +0800

    update event swagger api

--- Django_framework/event/views.py
+++ Django_framework/event/views.py
@@ -112,7 +112,37 @@ class HandleGetAllAndCreateEvent(generics.CreateAPIView):
                 'errors': serializer.errors
             }
             return JsonResponse(response_data, status=status.HTTP_400_BAD_REQUEST)
-
+    @swagger_auto_schema(
+        operation_summary='Update Event',
+        operation_description='Update an event by ID',
+        manual_parameters=[
+            openapi.Parameter(
+                name='event_id',
+                in_=openapi.IN_QUERY,
+                description='Event ID to update',
+                type=openapi.TYPE_INTEGER
+            )
+        ],
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'creator': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'event_name': openapi.Schema(type=openapi.TYPE_STRING),
+                'company_name': openapi.Schema(type=openapi.TYPE_STRING),
+                'hashtag': openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Schema(type=openapi.TYPE_STRING)),
+                'location': openapi.Schema(type=openapi.TYPE_STRING),
+                'event_date': openapi.Schema(type=openapi.FORMAT_DATETIME),
+                'scale': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'budget': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'detail': openapi.Schema(type=openapi.TYPE_STRING),
+                'create_datetime': openapi.Schema(type=openapi.FORMAT_DATETIME),
+                'update_datetime': openapi.Schema(type=openapi.FORMAT_DATETIME),
+                'delete_datetime': openapi.Schema(type=openapi.FORMAT_DATETIME, nullable=True),
+                'score': openapi.Schema(type=openapi.TYPE_INTEGER),
+            },
+            required=['id', 'creator', 'event_name', 'company_name', 'hashtag', 'location', 'event_date', 'scale', 'budget', 'detail', 'create_datetime', 'update_datetime', 'delete_datetime', 'score']  # Adjust as per your serializer requirements
+        )
+    )
     def put(self, request, *args, **kwargs):
         # Extract event_id from URL path
         event_id = request.query_params.get('event_id')
@@ -145,10 +175,57 @@ class HandleGetAllAndCreateEvent(generics.CreateAPIView):
                 'status': status.HTTP_404_NOT_FOUND, 
             }
             return JsonResponse(resp)
+    @swagger_auto_schema(
+        operation_summary='Delete Event',
+        operation_description='Delete an event by ID',
+        manual_parameters=[
+            openapi.Parameter(
+                name='event_id',
+                in_=openapi.IN_QUERY,
+                description='Event ID to delete',
+                type=openapi.TYPE_INTEGER
+            )
+        ]
+    )
+    def delete(self, request, *args, **kwargs):
+        # Extract event_id from URL path
+        event_id = request.query_params.get('event_id')
+        
+        if event_id:
+            try:
+                # Retrieve the event object from the database
+                event = Event.objects.get(id=event_id)
+                # Delete the event
+                event.delete()
+                return Response({'message': 'Event deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+            except Event.DoesNotExist:
+                # Return 404 response if event with specified ID doesn't exist
+                return Response({'error': "Event with specified ID not found"}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            resp = {
+                'data': list(data),
+                'error': "data with specified eventID not found",
+                'status': status.HTTP_404_NOT_FOUND, 
+            }
+            return JsonResponse(resp) 
 
 class HandleCreateParticipant(generics.CreateAPIView):
     queryset = Participant.objects.all()
     serializer_class = ParticipantSerializer
+    @swagger_auto_schema(
+        operation_summary='Get Event Info',
+        operation_description="""
+        Get all event info: http://34.81.121.53/:8000/api/event
+        Get event info by event ID:http://34.81.121.53/:8000/api/event/?event_id=1""",
+        manual_parameters=[
+            openapi.Parameter(
+                name='event_id',
+                in_=openapi.IN_QUERY,
+                description='EVENT ID',
+                type=openapi.TYPE_INTEGER
+            )
+        ]
+    )
     def get(self, request, *args, **krgs):
         event_id = request.query_params.get('event_id')
         user_id = request.query_params.get('user_id')
@@ -180,6 +257,19 @@ class HandleCreateParticipant(generics.CreateAPIView):
             }
             return JsonResponse(resp)
             
+    @swagger_auto_schema(
+        operation_summary='Join Event',
+        operation_description='POST http://34.81.121.53/:8000/api/eventInfo/',
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'event_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'user_id': openapi.Schema(type=openapi.TYPE_STRING)
+                # Add other properties of your Event model here
+            },
+            required=['event', 'user']  # Adjust as per your serializer requirements
+        )
+    )
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
