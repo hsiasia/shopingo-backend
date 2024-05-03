ae139a1b30a77ce3ccdf2b8e7e0c6e6ac523457e
hsiasia <hsiasiasiasia0829@gmail.com>
2024-05-04 02:10:39 +0800

    chore: sync mysql schema

--- Django_framework/user/models.py
+++ Django_framework/user/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    id = models.CharField(max_length=50, primary_key=True) 
-    name = models.CharField(max_length=16)
-    gmail = models.EmailField(max_length=256)
-    profile_pic = models.URLField(max_length=256)
-    score = models.IntegerField(default=5)
+    id = models.CharField(max_length=50, primary_key=True, null=False)
+    name = models.CharField(max_length=16, null=False)
+    gmail = models.EmailField(max_length=256, null=False)
+    profile_pic = models.URLField(max_length=256, null=False)
+    score = models.FloatField(default=5, null=False)
+    score_amounts = models.IntegerField(default=1, null=False)
 
     def __str__(self):
         return self.name
