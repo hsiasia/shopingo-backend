900267c457be94c1c481c0bacd40b78dfa8e5361
hsiasia <hsiasiasiasia0829@gmail.com>
2024-05-04 18:13:47 +0800

    chore: delete event score

--- Django_framework/event/serializers.py
+++ Django_framework/event/serializers.py
@@ -5,12 +5,11 @@ from .models import Participant
 class GetEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = ['id', 'creator', 'event_name', 'company_name', 'hashtag', 'location', 'event_date', 'scale', 'budget', 'detail', 'create_datetime', 'update_datetime', 'delete_datetime', 'score']
+        fields = ['id', 'creator', 'event_name', 'company_name', 'hashtag', 'location', 'event_date', 'scale', 'budget', 'detail', 'create_datetime', 'update_datetime', 'delete_datetime']
         extra_kwargs = {
             'create_datetime': {'required': False},  # Set required to False to allow null values
             'update_datetime': {'required': False},  # Set required to False to allow null values
             'dalete_datetime': {'required': False},  # Set required to False to allow null values
-            'score': {'required': False}  # Set required to False to allow null values
         }
 
 
