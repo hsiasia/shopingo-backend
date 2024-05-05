eb69ea8c162c62a54a77082e4916e3aa2356b853
r12725062 <r12725062@ntu.edu.tw>
2024-05-05 17:03:37 +0800

    fixed conflict while rebasing

--- Django_framework/event/models.py
+++ Django_framework/event/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from user.models import User  
 
 class Event(models.Model):
+
     id = models.AutoField(primary_key=True, null=False)
     creator = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
     event_name = models.CharField(max_length=32, null=False)
@@ -15,6 +16,8 @@ class Event(models.Model):
     create_datetime = models.DateTimeField(null=False, auto_now_add=True)
     update_datetime = models.DateTimeField(null=False, auto_now=True)
     delete_datetime = models.DateTimeField(null=True)
+    score = models.IntegerField(null=True)
+
 
     def __str__(self):
         return self.event_name
@@ -23,3 +26,6 @@ class Participant(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     score = models.IntegerField(null=True)
+
+
+  
--- Django_framework/event/urls.py
+++ Django_framework/event/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from event.views import  HandleGetAllAndCreateEvent
 from event.views import HandleCreateParticipant
+from django.urls import re_path
+from .consumers import EventUpdatesConsumer
 
 urlpatterns = [
     path('api/event/',  HandleGetAllAndCreateEvent.as_view(), name='HandleEvent-api'),
     path('api/eventInfo/', HandleCreateParticipant.as_view(), name='HandleCreateParticipant-api')
 ]
+"""
+websocket_urlpatterns = [
+    re_path(r'ws/event-updates/$', EventUpdatesConsumer.as_asgi()),
+]
+"""
