b216f3f270c90747fc9e520a06e9342f7e7a9c63
蔡見昇 <jason_tsai@p225.g4.ntu.edu.tw>
2024-05-06 02:01:01 +0800

    modify

--- Django_framework/googleAPIs/views.py
+++ Django_framework/googleAPIs/views.py
@@ -6,7 +6,6 @@ from rest_framework.parsers import JSONParser
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from googleAPIs.tool.tools import generate_download_signed_urls
-from event.models import Event, Image
 
 class GenerateSignedUrlsAPIView(generics.CreateAPIView):
     parser_classes = [JSONParser]
@@ -14,10 +13,9 @@ class GenerateSignedUrlsAPIView(generics.CreateAPIView):
     request_body = openapi.Schema(
         type=openapi.TYPE_OBJECT,
         properties={
-            'blob_names': openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Items(type=openapi.TYPE_STRING), description='List of blob names for which signed URLs are to be generated'),
-            'event_id': openapi.Schema(type=openapi.TYPE_STRING, description='the event_id')
+            'blob_names': openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Items(type=openapi.TYPE_STRING), description='List of blob names for which signed URLs are to be generated')
         },
-        required=['blob_names','event_id']  # Specify required fields
+        required=['blob_names']  # Specify required fields
     )
 
     # Define the successful response schema
@@ -70,23 +68,17 @@ class GenerateSignedUrlsAPIView(generics.CreateAPIView):
     )
     def post(self, request, *args, **kwargs):
         blob_names = request.data.get('blob_names')
-        event_id = request.data.get('event_id')
 
-        if not blob_names or not event_id:
+        if not blob_names:
             resp = {
                 'data': None,
-                'error': 'Missing required parameter: blob_names, event_id',
+                'error': 'Missing required parameter: blob_names',
                 'status': status.HTTP_400_BAD_REQUEST
             }
             return Response(resp, status=status.HTTP_400_BAD_REQUEST)
-
+        
         try:
-            event = Event.objects.get(id=event_id)
-
             urls = generate_download_signed_urls(blob_names)
-            for url_pair in urls:
-                gcs_static_url = url_pair[1]  
-                Image.objects.create(event=event, url=gcs_static_url)
                 
             resp = {
                 'data': urls,
@@ -94,14 +86,6 @@ class GenerateSignedUrlsAPIView(generics.CreateAPIView):
                 'status': status.HTTP_201_CREATED
             }
             return Response(resp, status=status.HTTP_201_CREATED)
-        
-        except Event.DoesNotExist:
-            resp = {
-                'data': None,
-                'error': f'Event with ID {event_id} not found',
-                'status': status.HTTP_400_BAD_REQUEST
-            }
-            return Response(resp, status=status.HTTP_400_BAD_REQUEST)
 
         except Exception as e:
             resp = {
