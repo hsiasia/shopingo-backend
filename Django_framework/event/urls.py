96f8d1ed68814f3dc98508e84b0e59b25beee539
ZG926 <45334576+ZG926@users.noreply.github.com>
2024-05-11 12:05:44 +0800

    finished saved event' (#28)
    Co-authored-by: r12725062 <r12725062@ntu.edu.tw>

--- Django_framework/event/urls.py
+++ Django_framework/event/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 
 from django.urls import re_path
-
-from event.views import  HandleGetAllAndCreateEvent, HandleCreateParticipant,HandleGetEventsByStatus, UpdateEventImage
+from .consumers import EventUpdatesConsumer
+from event.views import  HandleGetAllAndCreateEvent, HandleCreateParticipant,HandleGetEventsByStatus, UpdateEventImage,HandleSavedEvent
 
 
 urlpatterns = [
     path('api/event/images',  UpdateEventImage.as_view(), name='UpdayeEventImage-api'),
     path('api/event/',  HandleGetAllAndCreateEvent.as_view(), name='HandleEvent-api'),
     path('api/userEvent/',  HandleGetEventsByStatus.as_view(), name='HandleEventByStatus-api'),
-    path('api/eventInfo/', HandleCreateParticipant.as_view(), name='HandleCreateParticipant-api')
+    path('api/eventInfo/', HandleCreateParticipant.as_view(), name='HandleCreateParticipant-api'),
+    path('api/saveEvent/', HandleSavedEvent.as_view(), name='SavedEvent-api')
 ]
 """
 websocket_urlpatterns = [
