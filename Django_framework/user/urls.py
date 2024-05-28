618bbd36fb919aaff0ebab83b57a71aa73115c27
蔡見昇 <jason_tsai@p225.g4.ntu.edu.tw>
2024-05-28 23:06:30 +0800

    fix: add urls.py static file route

--- Django_framework/user/urls.py
+++ Django_framework/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from user.views import GetUserByID,CreateUserAccount,UpdateUserScore,GetUserScoreData
 
+
 urlpatterns = [
     path('api/user/scoreHistory', GetUserScoreData.as_view()),
     path('api/user/score', UpdateUserScore.as_view()),    
     path('api/user/login', CreateUserAccount.as_view()),
     path('api/user/', GetUserByID.as_view())   
-]
+] 
